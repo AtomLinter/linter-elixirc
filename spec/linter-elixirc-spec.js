c0c27650c3eb528d930c4a261c646c30918e63ba
Landon Abney <Landonabney@gmail.com>
2017-09-19 14:07:38 -0700

    :shirt: Fix linting issues

--- spec/linter-elixirc-spec.js
+++ spec/linter-elixirc-spec.js
@@ -4,6 +4,8 @@ import { join } from 'path';
 // eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved
 import { remove } from 'fs-extra';
 
+const { lint } = require('../lib/init.js').provideLinter();
+
 const validPathElixirc = join(__dirname, 'fixtures', 'elixirc', 'valid.ex');
 const warningPathElixirc = join(__dirname, 'fixtures', 'elixirc', 'warning.ex');
 const errorMode1PathElixirc = join(__dirname, 'fixtures', 'elixirc', 'error-mode1.ex');
@@ -19,18 +21,14 @@ remove(mixBuildDirectory);
 describe('The elixirc provider for Linter', () => {
   describe('when not working inside a Mix project', () => {
     describe('and using the standard configuration', () => {
-      let lint;
-
       beforeEach(() => {
-        lint = require('../lib/init.js').provideLinter().lint;
         atom.workspace.destroyActivePaneItem();
 
         waitsForPromise(() =>
           Promise.all([
             atom.packages.activatePackage('linter-elixirc'),
             atom.packages.activatePackage('language-elixir'),
-          ]),
-        );
+          ]));
       });
 
       it('works with mode 1 errors', () => {
@@ -43,8 +41,7 @@ describe('The elixirc provider for Linter', () => {
             expect(messages[0].excerpt).toBe('(ArgumentError) Dangerous is not available');
             expect(messages[0].location.file).toBe(errorMode1PathElixirc);
             expect(messages[0].location.position).toEqual([[1, 0], [1, 32]]);
-          }),
-        );
+          }));
       });
 
       it('works with mode 2 errors', () => {
@@ -57,8 +54,7 @@ describe('The elixirc provider for Linter', () => {
             expect(messages[0].excerpt).toBe('(CompileError) module Usefulness is not loaded and could not be found');
             expect(messages[0].location.file).toBe(errorMode2PathElixirc);
             expect(messages[0].location.position).toEqual([[3, 2], [3, 20]]);
-          }),
-        );
+          }));
       });
 
       it('works with warnings', () => {
@@ -70,8 +66,7 @@ describe('The elixirc provider for Linter', () => {
             expect(messages[0].excerpt).toBe('variable "prepare_for_call" does not exist and is being expanded to "prepare_for_call()", please use parentheses to remove the ambiguity or change the variable name');
             expect(messages[0].location.file).toBe(warningPathElixirc);
             expect(messages[0].location.position).toEqual([[20, 4], [20, 20]]);
-          }),
-        );
+          }));
       });
 
       it('works with .exs files', () => {
@@ -83,34 +78,28 @@ describe('The elixirc provider for Linter', () => {
             expect(messages[0].excerpt).toBe('function simple_function/0 is unused');
             expect(messages[0].location.file).toBe(exsFilePathElixirc);
             expect(messages[0].location.position).toEqual([[1, 2], [1, 25]]);
-          }),
-        );
+          }));
       });
 
       it('finds nothing wrong with a valid file', () => {
         waitsForPromise(() =>
           atom.workspace.open(validPathElixirc).then(editor => lint(editor)).then((messages) => {
             expect(messages.length).toBe(0);
-          }),
-        );
+          }));
       });
     });
   });
 
   describe('when working inside a Mix project', () => {
     describe('and using the standard configuration', () => {
-      let lint;
-
       beforeEach(() => {
-        lint = require('../lib/init.js').provideLinter().lint;
         atom.workspace.destroyActivePaneItem();
 
         waitsForPromise(() =>
           Promise.all([
             atom.packages.activatePackage('linter-elixirc'),
             atom.packages.activatePackage('language-elixir'),
-          ]),
-        );
+          ]));
       });
 
       it('works with mode 2 errors', () => {
@@ -123,8 +112,7 @@ describe('The elixirc provider for Linter', () => {
             expect(messages[0].excerpt).toBe('(CompileError) Identicon.Image.__struct__/1 is undefined, cannot expand struct Identicon.Image');
             expect(messages[0].location.file).toBe(errorMode2PathMix);
             expect(messages[0].location.position).toEqual([[11, 4], [11, 30]]);
-          }),
-        );
+          }));
       });
 
       it('works with .exs files', () => {
@@ -136,27 +124,22 @@ describe('The elixirc provider for Linter', () => {
             expect(messages[0].excerpt).toBe('function simple_function/0 is unused');
             expect(messages[0].location.file).toBe(exsFilePathMix);
             expect(messages[0].location.position).toEqual([[1, 2], [1, 25]]);
-          }),
-        );
+          }));
       });
     });
   });
 });
 
 describe('when using the setting forceElixirc', () => {
-  let lint;
-
   beforeEach(() => {
     atom.config.set('linter-elixirc.forceElixirc', true);
-    lint = require('../lib/init.js').provideLinter().lint;
     atom.workspace.destroyActivePaneItem();
 
     waitsForPromise(() =>
       Promise.all([
         atom.packages.activatePackage('linter-elixirc'),
         atom.packages.activatePackage('language-elixir'),
-      ]),
-    );
+      ]));
   });
 
   it('works with warnings', () => {
@@ -168,7 +151,6 @@ describe('when using the setting forceElixirc', () => {
         expect(messages[0].excerpt).toBe('(CompileError) Identicon.Image.__struct__/1 is undefined, cannot expand struct Identicon.Image');
         expect(messages[0].location.file).toBe(errorMode2PathMix);
         expect(messages[0].location.position).toEqual([[11, 4], [11, 30]]);
-      }),
-    );
+      }));
   });
 });
